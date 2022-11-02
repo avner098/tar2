a57b8f51ddf93da64b394b80a7b0e94ff2a9f17a
Matantu <matanturjeman123@gmail.com>
2022-11-02 10:40:17 +0200

    commit something

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-///
+//
+//
 public class TestThreadCheckArray {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
@@ -38,7 +39,7 @@ public class TestThreadCheckArray {
 			}
 			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			for(int index = 0; index < sd.getArray().size() ; index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
